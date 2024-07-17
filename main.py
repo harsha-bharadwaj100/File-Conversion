1abe33cab83e836d9046398f8d2724ca0227d0f9
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-07-17 16:43:30 +0530

    created a batch file to run it!

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Hello, Convertor!")
+try:
+    import docx2pdf as dp
+
+    print("Success")
+    input()
+except ImportError as I:
+    print(I)
+    input()
+    print("Failure")
+finally:
+    print("DONE")
+    input()
---
+++ st.bat
@@ -0,0 +1,6 @@
+@REM "env\Scripts\activate.bat" && python main.py
+
+call "env\Scripts\activate.bat"
+python main.py
+
+@REM call "C:\D-sim\pyprojs\File Conversion\env\Scripts\python.exe" "C:\D-sim\pyprojs\File Conversion\main.py"
