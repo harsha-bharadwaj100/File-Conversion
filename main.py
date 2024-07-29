5141103a7357dd0b3e2e22d8657427d216767823
Harsha Bharadwaj <88173012+harsha-bharadwaj100@users.noreply.github.com>
2024-07-29 23:53:57 +0530

    installations + more knowledge abt pipenv

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 print("Hello, Convertor!")
-try:
-    import docx2pdf as dp
-
-    print("Success")
-    input()
-except ImportError as I:
-    print(I)
-    input()
-    print("Failure")
-finally:
-    print("DONE")
-    input()
+import pdf2docx
+import docx2pdf
